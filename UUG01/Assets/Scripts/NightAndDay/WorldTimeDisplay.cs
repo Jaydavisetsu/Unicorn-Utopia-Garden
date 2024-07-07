6492e3ab4bd62826e6f0bc9b0fe07aa1d49fb9c3
Sandyy88 <mejiamejiasandra8@gmail.com>
2024-07-06 20:21:48 -0400

    Dynamic Ordering
    Fixed the sorting and rendering of things.

---
+++ UUG01/Assets/Scripts/NightAndDay/WorldTimeDisplay.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+[RequireComponent(typeof(TMP_Text))]
+public class WorldTimeDisplay : MonoBehaviour
+{
+    [SerializeField] private WorldTime _worldTime;
+    private TMP_Text _text;
+
+    private void Awake()
+    {
+        _text = GetComponent<TMP_Text>();
+        _worldTime.WorldTimeChanged += OnWorldTimeChanged;
+    }
+
+    private void OnDestroy()
+    {
+        _worldTime.WorldTimeChanged -= OnWorldTimeChanged;
+    }
+
+    private void OnWorldTimeChanged(object sender, TimeSpan newTime)
+    {
+        _text.SetText(newTime.ToString(@"hh\:mm"));
+    }
+
+}
--- UUG01/Assets/Scripts/WorldTimeConstants.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-
-
-public static class WorldTimeConstants
-{
-
-    public const int MinutesInDay = 1440;
-}
